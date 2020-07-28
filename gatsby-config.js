40efde094becabac266be365516a70bb1522d8af
nkonada <knc@outlook.in>
2020-07-29 00:12:40 +0530

    page creator samples for blogs, articles and tutorials

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,45 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blogs`,
+        path: `${__dirname}/src/content/blogs/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/src/content/articles/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `tutorials`,
+        path: `${__dirname}/src/content/tutorials/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/content/blogs/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/content/articles/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/content/tutorials/`,
+      },
+    },
     `gatsby-plugin-mdx`,
   ],
 }
