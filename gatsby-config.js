18c76412604919d53e340fd0508af8fca01f82f4
nkonada <knc@outlook.in>
2020-07-29 00:00:34 +0530

    added mdx configuration for pages directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+  ],
 }
