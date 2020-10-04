8d654b0a55d59eb3d9356101ebb855c1e6cff4ae
nagachaitanya.konada <nagachaitanya.konada@servicenow.com>
2020-10-04 21:51:30 +0530

    margins removed for headers

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -19,7 +19,7 @@ html, body {
 h1, h2, h3, h4, h5, h6 {
     font-family: ${fonts.heading};
     font-weight: 900;
-    margin: .5em 0 .5em;
+    margin: 0;
     padding-top: .5em;
     line-height: 1.5;
     color: ${colors.primary}
@@ -95,6 +95,35 @@ img {
     padding-left: 2.8em;
     overflow: initial;
   }
+
+a {
+  color: ${props => props.color || colors.secondary};
+  font-family: ${fonts.body};
+  font-weight: 800;
+  text-decoration: none;
+  position: relative;
+  display: inline-block;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 0;
+    transform: scaleX(0);
+    width: 100%;
+    height: 3px;
+    background-color: ${props => props.underlineColor || `currentColor`};
+    transition: transform 200ms ease-in-out;
+    transform-origin: right;
+  }
+
+  &:focus::after,
+  &:active::after,
+  &:hover::after,
+  &.active::after {
+    transform: scaleX(1);
+    transform-origin: left;
+  }
+}
 
 `
 
